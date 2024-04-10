1d26615344b750302e754b332c90d63d31d38dc0
chizel9000 <chizel9000@users.noreply.github.com>
2024-04-09 20:15:37 -0400

    Add files via upload

--- mw.js
+++ mw.js
@@ -1,5 +1,66 @@
 window.addEventListener("load", (event) => { scale(); });
 window.addEventListener("resize", (event) => { scale(); });
+
+const deta = [
+    {name:"Epoch Melchior", desc:"Time machine lorem dolor sit amet lorem dolor sit amet."},
+    {name:"Lucca Toma", desc:"Genius inventor lorem dolor sit amet lorem dolor sit amet."},
+    {name:"Crono Player", desc:"Stalwort swordsman lorem dolor sit amet lorem dolor sit amet."},
+    {name:"Marle Nadia", desc:"Rambunctious princess lorem dolor sit amet lorem dolor sit amet."},
+    {name:"Frog Glenn", desc:"Frog warrior lorem dolor sit amet lorem dolor sit amet."},
+    {name:"Chief Ayla", desc:"Cave babe lorem dolor sit amet lorem dolor sit amet."},
+    {name:"Magus Janus", desc:"Magic master lorem dolor sit amet lorem dolor sit amet."},
+    {name:"Robo Proto", desc:"Future machine lorem dolor sit amet lorem dolor sit amet."},
+    {name:"Crono Player", desc:"Stalwort swordsman lorem dolor sit amet lorem dolor sit amet."},
+    {name:"Marle Nadia", desc:"Rambunctious princess lorem dolor sit amet lorem dolor sit amet."}
+    
+];
+var current = 2;
+var last = 1;
+var next = 3;
+//var scrollcounter = 0;
+const element = document.getElementById("scroll");
+var namep = document.getElementById("nu");
+var info = document.getElementById("deta");
+var list = document.querySelectorAll(".slide");
+    console.log(list);
+
+element.onclick = (event) => {
+    //scrollcounter++;
+    //console.log(scrollcounter);
+    
+    
+    if(next < list.length)
+    {
+        current ++;
+        last ++;
+        next ++;
+    }
+    else
+        {current = 0; last=-1; next=1;}
+    namep.innerHTML = deta[current].name;
+    info.innerHTML = deta[current].desc;
+    
+list.forEach((item, index) => {
+   if(index < current)
+    item.style.left = -20+"%"; 
+    else if (index > current) 
+        item.style.left = 80+"%"; 
+    else item.style.left = 20+"%"; 
+        
+    if(index == current) item.classList.add("activeSlide");
+    else item.classList.remove("activeSlide");
+    if(index == last || index == next) item.classList.add("nextSlide");
+    else item.classList.remove("nextSlide");
+}); 
+    
+}
+    namep.innerHTML = deta[current].name;
+    info.innerHTML = deta[current].desc;
+/*
+list.forEach(item => {
+   item.style.left = list.indexOf(item)*10+"px"; 
+});  */
+
 function scale(){
 const viewWIDTH = Math.floor(innerWidth);
 const viewHEIGHT = Math.floor(innerHeight);
@@ -7,6 +68,39 @@ var bod = document.body;
 bod.style.height = viewHEIGHT;
 bod.style.width = viewWIDTH;
 
+
+    
+list.forEach((item, index) => {
+   if(index < current)
+    item.style.left = -20+"%"; 
+    else if (index > current) 
+        item.style.left = 80+"%"; 
+    else item.style.left = 20+"%"; 
+        
+    if(index == current) item.classList.add("activeSlide");
+    else item.classList.remove("activeSlide");
+    if(index == last || index == next) item.classList.add("nextSlide");
+    else item.classList.remove("nextSlide");
+}); 
+    /*
+for(var i=0; i < list.length; i++){
+    
+    list[i].style.left = (i)*100+"px"; 
+    
+    if(i==last || i==next){
+            list[i].classList.add("nextSlide");
+    }
+    if(i>1 && i<7){
+        //list[i].style.opacity = 1;
+        if(i==current)
+        list[i].classList.add("activeSlide");
+        
+        }
+        //else {
+        //list[i].style.opacity = 0.25;
+        //}
+}
+    */
 var top = document.getElementById("tc");
 top.style.height = viewHEIGHT*0.25+"px";    
 var logo = document.getElementById("logo");
