aea9f967db5393c30838dd3bb5b1649aee57c2e7
chizel9000 <chizel9000@users.noreply.github.com>
2024-05-21 04:26:55 -0400

    Add files via upload

--- mw.js
+++ mw.js
@@ -181,7 +181,10 @@ else{
 //crew.style.height = viewHEIGHT*1.25+"px";
     
 if(viewWIDTH < 500){
-   
+   var artist = document.getElementById("artist");
+    if(artist)
+        {artist.classList.remove("hwcard");
+        artist.classList.add("card");}
 var logo = document.getElementById("special");
     if(logo)
 logo.style.height = viewHEIGHT*0.5+"px";
@@ -197,7 +200,10 @@ concern.style.height = viewHEIGHT*0.5+"px";
 foot.style.height = viewHEIGHT*0.5+"px";
 }
 else{
-    
+    var artist = document.getElementById("artist");
+    if(artist)
+        {artist.classList.remove("card");
+        artist.classList.add("hwcard");}
     var logo = document.getElementById("special");
     if(logo)
         logo.style.height = viewHEIGHT+"px";
@@ -250,6 +256,60 @@ for(var i=0; i < list.length; i++){
     
 
 }
+var count = 6;    
+//var lengthcheck = 6;
+var slices = document.querySelectorAll("#backdrop > .bdslice > img");
+    console.log(slices);
+    var gallery = document.querySelectorAll("#hg > img");
+    console.log(gallery);
+function listCopy(){
+   // var gt = [];
+    
+    //slist.length + glist.length
+   // gt = slices;
+  //  gt = gallery;
+  //  console.log(gt);
+    
+
+    slices.forEach((item, index)=>{
+        //if(lengthcheck < gallery.length-1){
+          //  lengthcheck ++;
+        if(count == gallery.length)count=0;
+        console.log(count);
+            let seed = gallery[count].src;
+        console.log(seed);
+            item.src = seed;
+        
+        count++;
+       // item.src = index+slices.length + ""
+          //  console.log(lengthcheck,gallery.length);
+            
+       // }
+      //  else {lengthcheck = 0;
+      //       let seed = gallery[lengthcheck].src;
+     //   item.src = seed;
+     //        }
+     //   
+        
+            
+    });
+    
+    
+   // if(lengthcheck >= gallery.length)
+     //   {count= 0; lengthcheck = 0;} //intvalID.clearInterval();
+       // else
+         //   count++;
+
+   // for(var i = 0; i < gt; i++){
+   //     slices.forEach((item)=>{
+   //         let seed = gallery[index+i].src;
+   //         item.src = seed;
+   //     });
+   // }
+
+
+}
+
 
 function toggleMenu(){
     console.log("rawr");
@@ -308,4 +368,5 @@ function toggleGallery(){
 }
 
 
-
+let intvalID;
+intvalID = setInterval(listCopy,3000);
